8dbb8823fb54653376db3e700c4c6ea13391bc7c
zed-mocean <70882784+zed-mocean@users.noreply.github.com>
2021-04-07 10:53:32 +0800

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'MoceanSMS Login',
-    'description' => '2FA SMS Login.',
+    'description' => 'MoceanAPI 2FA Login Typo3 Extension allows frontend users to opt in to SMS 2FA verification for login. This extension requires static templates(Bootstrap Package: Full Package and Typoscript Form) to function. ',
     'category' => 'plugin',
     'author' => 'moceanapi',
     'author_company' => 'Micro Ocean Technologies',
